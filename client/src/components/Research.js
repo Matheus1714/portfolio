0347fd7b5f550774f689d44d17d48c7b3ec55fed
Matheus Mota <matheussilvamartins1714@gmail.com>
2020-02-08 01:41:02 -0300

    create text research

---
+++ client/src/components/Research.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import NetworkWifiIcon from '@material-ui/icons/NetworkWifi';
+import InsertChartIcon from '@material-ui/icons/InsertChart';
+
+const useStyles = makeStyles( theme => ({
+    root: {
+        display: 'flex',
+        margin: theme.spacing(1),
+    },
+}))
+
+export default function Research() {
+    const classes = useStyles();
+    return (
+        <div className={classes.root}>
+            <div>
+                <h1>Research</h1>
+                <div style={{display:'flex'}}>
+                    <NetworkWifiIcon/>
+                    <h4 style={{marginTop:4, marginLeft:10}}>Network Encryption</h4>
+                </div>
+                <div style={{display:'flex'}}>
+                    <InsertChartIcon/>
+                    <h4 style={{marginTop:4, marginLeft: 10}}>Artificial Intelligence</h4>
+                </div>
+            </div>
+        </div>
+    )
+}
