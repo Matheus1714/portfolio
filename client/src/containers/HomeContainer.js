0c2b7f2c898053ab3ce817a15e93704bc75c14ec
Matheus Mota <matheussilvamartins1714@gmail.com>
2020-02-08 02:13:29 -0300

    message visits

--- client/src/containers/HomeContainer.js
+++ client/src/containers/HomeContainer.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class HomeContainer extends React.Component {
     render(){
         return(
-            <h1>Home</h1>
+            <h3>This web site is a resume of all that create with web developer.</h3>
         )
     }
 }
